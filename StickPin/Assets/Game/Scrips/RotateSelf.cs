523d6f55a099adb29abefefe05fd28436c7efc68
1305779886 <1305779886@qq.com>
2019-02-26 11:14:54 +0800

    见缝插针

---
+++ StickPin/Assets/Game/Scrips/RotateSelf.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 90;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //顺时针旋转
+        transform.Rotate(new Vector3(0, 0, -speed * Time.deltaTime));
+    }
+}
