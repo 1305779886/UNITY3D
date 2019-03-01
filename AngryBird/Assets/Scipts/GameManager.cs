94ff03e5a497bd9f4218470a104fd1552c4b5fa6
1305779886 <1305779886@qq.com>
2019-03-01 18:20:04 +0800

    愤怒的小鸟
    只有第一关，已实现暂停、重新开始以及退出界面功能；
    小鸟有三个：红色普通鸟，绿色回旋鸟和黄色冲锋鸟

---
+++ AngryBird/Assets/Scipts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Bird> birds;
+    public List<Pig> pig;
+    public static GameManager _instance;
+    private Vector3 originPos;//初始的位置
+
+    public GameObject win;
+    public GameObject lose;
+    public GameObject[] starts;
+
+    private void Awake()
+    {
+        _instance = this;
+        if (birds.Count > 0)
+        {
+            originPos = birds[0].transform.position;
+        }
+        originPos = birds[0].transform.position;
+    }
+    private void Start()
+    {
+        Initialized();
+    }
+    /// <summary>
+    /// 初始化小鸟
+    /// </summary>
+    private void Initialized()
+    {
+        for (int i = 0; i < birds.Count; i++)
+        {
+            if (i == 0)//第一只小小鸟
+            {
+                birds[i].transform.position = originPos;
+                birds[i].enabled = true;
+                birds[i].sp.enabled = true;
+            }
+            else
+            {
+                birds[i].enabled = false;
+                birds[i].sp.enabled = false;
+            }
+        }
+    }
+    /// <summary>
+    /// 判断游戏逻辑
+    /// </summary>
+    public void NextBird()
+    {
+        if (pig.Count > 0)
+        {
+            if (birds.Count > 0)
+            {
+                //启动下一只小鸟
+                Initialized();
+            }
+            else
+            {
+                //GG
+                lose.SetActive(true);
+            }
+        }
+        else
+        {
+            //赢了
+            win.SetActive(true);
+        }
+    }
+    public void ShowStars()
+    {
+        StartCoroutine("Show");
+    }
+    IEnumerator Show()
+    {
+        for(int i = 0; i < birds.Count +1; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            starts[i].SetActive(true);
+        }
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
